90ddd8c46ada8449321cdeb57a06459259b3b15e
Humbleking24 <111560964+Humbleking24@users.noreply.github.com>
2025-04-08 03:32:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 const sendmessageButton = document.querySelector("#send-message");
 const fileInput = document.querySelector("#file-input");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const fileCancelButton = document.querySelector("#file-cancel");
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "AIzaSyACiMxia9CFU21w0YBWRQBivGHBhOVOE8M";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
@@ -44,6 +46,7 @@ const generateBotResponse = async (incomingMessageDiv) => {
         messageElement.innerText = error.message;
         messageElement.style.color = "#ff0000";
     }finally {
+        userData.file = {}
         incomingMessageDiv.classList.remove("thinking")
         chatBody.scrollTo({top: chatBody.scrollHeight, behavior: "smooth"})
     }
@@ -53,9 +56,11 @@ const handleOutgoingMessage = (e) => {
     e.preventDefault();
     userData.message = messageInput.value.trim();
     messageInput.value="";
+    fileUploadWrapper.classList.remove("file-uploaded")
 
 
-    const messageContent = `<div class="message-text"></div>`
+    const messageContent = `<div class="message-text"></div>
+                            ${userData.file.data ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="attachment" />`: ""}`;
     const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
     outgoingMessageDiv.querySelector(".message-text").textContent=userData.message;
     chatBody.appendChild(outgoingMessageDiv);
@@ -92,6 +97,8 @@ fileInput.addEventListener("change", () => {
 
     const reader = new FileReader();
     reader.onload=(e)=>{
+        fileUploadWrapper.querySelector("img").src = e.target.result
+        fileUploadWrapper.classList.add("file-uploaded")
         const base64String = e.target.result.split(",")[1];
 
         userData.file ={
@@ -103,6 +110,31 @@ fileInput.addEventListener("change", () => {
 
     reader.readAsDataURL(file);
 })
+
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded")
+})
+
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+        const {selectionStart: start, selectionEnd: end} = messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus()
+    },
+    onClickOustide: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.add("show-emoji-picker")
+        }else{
+            document.body.classList.remove("show-emoji-picker")
+        }
+    }
+})
+
+document.querySelector(".chat-form").appendChild(picker)
 
 sendmessageButton.addEventListener("click", (e) => handleOutgoingMessage(e))
 document.querySelector("#file-upload").addEventListener("click", () => fileInput.click())
